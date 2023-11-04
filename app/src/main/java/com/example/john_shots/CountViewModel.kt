1cac921e0c33139d2ad968bc8589f614d642551a
Georgios <farmaker47@gmail.com>
2023-11-04 07:47:47 +0200

    Add composable, viewmodel and UI elements.

---
+++ app/src/main/java/com/example/john_shots/CountViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.john_shots
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class CountViewModel(application: Application) : AndroidViewModel(application) {
+
+}
---
+++ app/src/main/java/com/example/john_shots/composables/CountComposable.kt
@@ -0,0 +1,86 @@
+package com.example.john_shots.composables
+
+import android.Manifest
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.john_shots.CountViewModel
+import com.example.john_shots.R
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionStatus
+import com.google.accompanist.permissions.rememberPermissionState
+
+@OptIn(ExperimentalPermissionsApi::class, ExperimentalComposeUiApi::class)
+@Composable
+fun CountComposable(
+    modifier: Modifier = Modifier,
+    viewModel: CountViewModel = viewModel()
+) {
+    // Check for permissions.
+    val permissions = rememberPermissionState(
+        Manifest.permission.RECORD_AUDIO
+    )
+    if (permissions.status != PermissionStatus.Granted) {
+        SideEffect { permissions.launchPermissionRequest() }
+    }
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        Column(
+            modifier = Modifier
+                .width(400.dp)
+                .height(340.dp)
+                .verticalScroll(state = rememberScrollState())
+                .align(Alignment.Center)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.john),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(240.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .align(Alignment.CenterHorizontally)
+                    .clickable { /* No op */ },
+                contentScale = ContentScale.Fit
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "1",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(8.dp),
+                color = Color.White,
+                fontSize = 80.sp
+            )
+        }
+    }
+}
